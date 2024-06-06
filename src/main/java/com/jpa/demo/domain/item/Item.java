88368a0194ff09ab9418a42c682419a5625c2873
SooBin_Lee <i_romay@naver.com>
2024-06-06 18:00:06 +0900

    Feat : Order, OrderItem 엔티티 구현

--- src/main/java/com/jpa/demo/domain/item/Item.java
+++ src/main/java/com/jpa/demo/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.jpa.demo.domain.item;
 
 import com.jpa.demo.domain.Category;
+import com.jpa.demo.exception.NotEnoughStockException;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -27,4 +28,21 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
+
+    //비즈니스 로직
+    /**
+     * stock 증가
+     * **/
+    public void addStock(int stockQuantity) {
+        this.stockQuantity += stockQuantity;
+    }
+
+    public void removeStock(int stockQuantity) {
+        int restStock = this.stockQuantity - stockQuantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("재고가 존재하지 않습니다.");
+        }
+        this.stockQuantity = restStock;
+
+    }
 }
---
+++ src/main/java/com/jpa/demo/service/ItemService.java
@@ -0,0 +1,31 @@
+package com.jpa.demo.service;
+
+import com.jpa.demo.domain.item.Item;
+import com.jpa.demo.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long id) {
+        return itemRepository.findOne(id);
+    }
+
+}
